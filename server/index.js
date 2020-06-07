92adf75c7efde7f7a64ddde427ab0aa7c254ba4d
Connie Lok <conniehlok@gmail.com>
2020-06-06 22:15:03 -0400

    static files directories

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.use('/api', require('./api'));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
-app.use(express.static(path.join(__dirname, 'build')));
 
 app.use((err, req, res, next) => {
   console.error(err);
