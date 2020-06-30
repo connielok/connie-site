2ae0540778c31e528f31223bfc1e35521181e028
Connie Lok <conniehlok@gmail.com>
2020-06-29 21:51:14 -0400

    style: added image on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { Typography, Container } from '@material-ui/core';
 import Icons from './Icons';
+import homeImage from './img/home-girl-interests.png';
 
 const Home = () => {
   return (
-    <div
-      className="home"
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        height: '90vh',
-      }}
-    >
-      <Container maxWidth="md">
+    <div className="home">
+      <Container
+        maxWidth="md"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          height: '90vh',
+        }}
+      >
         <Typography variant="h1" align="center">
           Connie Lok
         </Typography>
         <Typography variant="h4" align="center">
           Software Engineer
         </Typography>
+        <img
+          src={homeImage}
+          alt="girl coder and other interests"
+          style={{ height: '30rem' }}
+        />
         <Icons />
       </Container>
     </div>
